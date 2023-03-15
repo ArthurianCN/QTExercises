140f9cb4b0ab09765737b29f52008ef4067ce4da
Arthurian <zhuanxinxin@aliyun.com>
2023-03-15 22:50:59 +0800

    调整布局；新增一个lable显示比较结果

--- FileMD5/widget.cpp
+++ FileMD5/widget.cpp
@@ -29,61 +29,56 @@ const QString GetFileMd5(const QString &path)
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
+    ,m_pLayoutMain(new QGridLayout(this))
+    ,m_lableResult(new QLabel(this))
 {
     this->resize( QSize( 400, 100 ));
-    m_pLayoutMain = new QVBoxLayout(this);
     m_lableHash1 = new QLabel( this);
-    m_pLayoutMain->addWidget(m_lableHash1);
+    m_pLayoutMain->addWidget(m_lableHash1, 0, 0, 1, 5);
     m_LineEditCustomFilePath1 = new QLineEditCustom(this);
-    m_pLayoutMain->addWidget(m_LineEditCustomFilePath1);
+    m_pLayoutMain->addWidget(m_LineEditCustomFilePath1, 1, 0, 1, 5);
 
     m_lableHash2 = new QLabel(this);
-    m_pLayoutMain->addWidget(m_lableHash2);
+    m_pLayoutMain->addWidget(m_lableHash2, 2, 0, 1, 5);
     m_LineEditCustomFilePath2 = new QLineEditCustom(this);
-    m_pLayoutMain->addWidget(m_LineEditCustomFilePath2);
+    m_pLayoutMain->addWidget(m_LineEditCustomFilePath2, 3, 0, 1, 5);
 
     m_pBtnVerify = new QPushButton("Verify", this);
-    pHLayoutBtn = new QHBoxLayout(this);
-    pHLayoutBtn->addStretch();
-    pHLayoutBtn->addWidget(m_pBtnVerify);
-    pHLayoutBtn->addStretch();
-    m_pLayoutMain->addLayout(pHLayoutBtn);
+    m_pLayoutMain->addWidget(m_lableResult, 4, 0, 1, 1);
+    m_pLayoutMain->addWidget(m_pBtnVerify, 4, 2, 1, 1);
 
-    setLayout(m_pLayoutMain);
-    connect(m_LineEditCustomFilePath1, &QLineEdit::textChanged, this, &Widget::onLineEditChanged);
-    connect(m_LineEditCustomFilePath2, &QLineEdit::textChanged, this, &Widget::onLineEditChanged);
+    connect(m_LineEditCustomFilePath1, &QLineEdit::textChanged, [=](){
+        m_lableHash1->setText(GetFileMd5(m_LineEditCustomFilePath1->text().toUtf8().data()));
+    }
+    );
+    connect(m_LineEditCustomFilePath2, &QLineEdit::textChanged, [=](){
+        m_lableHash2->setText(GetFileMd5(m_LineEditCustomFilePath2->text().toUtf8().data()));
+    }
+    );
     connect(m_pBtnVerify, &QPushButton::clicked, this, &Widget::onBtnVerify);
 }
 
 Widget::~Widget()
 {
-    //    delete ui;
-}
-
-void Widget::onLineEditChanged()
-{
-    QLineEditCustom* lineEditFile = qobject_cast<QLineEditCustom*>(sender());
-    if(lineEditFile == m_LineEditCustomFilePath1)
-    {
-         m_lableHash1->setText(GetFileMd5(m_LineEditCustomFilePath1->text().toUtf8().data()));
-    }
-    else
-    {
-        m_lableHash2->setText(GetFileMd5(m_LineEditCustomFilePath2->text().toUtf8().data()));
-    }
 }
 
 void Widget::onBtnVerify()
 {
     if(0 == m_lableHash1->text().compare(m_lableHash2->text()))
     {
-        QMessageBox::information(NULL, "Title", "Same",
-                                 QMessageBox::Ok, QMessageBox::Ok);
+        QPalette palette;
+        palette.setColor(QPalette::Background, QColor(0, 255, 0));
+        m_lableResult->setAutoFillBackground(true);  //一定要这句，否则不行
+        m_lableResult->setGeometry(m_lableResult->x(), m_lableResult->y(), m_pBtnVerify->size().width(), m_pBtnVerify->size().height());
+        m_lableResult->setPalette(palette);
     }
     else
     {
-        QMessageBox::information(NULL, "Title", "Not Same",
-                                 QMessageBox::Ok, QMessageBox::Ok);
+        QPalette palette;
+        palette.setColor(QPalette::Background, QColor(255, 0, 0));
+        m_lableResult->setAutoFillBackground(true);  //一定要这句，否则不行
+        m_lableResult->setGeometry(m_lableResult->x(), m_lableResult->y(), m_pBtnVerify->size().width(), m_pBtnVerify->size().height());
+        m_lableResult->setPalette(palette);
     }
 }
 
--- FileMD5/widget.h
+++ FileMD5/widget.h
@@ -3,15 +3,10 @@
 
 #include <QWidget>
 #include <QPushButton>
-#include <QHBoxLayout>
-#include <QVBoxLayout>
+#include <QGridLayout>
 #include <QCryptographicHash>
 #include <QLabel>
 #include "qlineeditcustom.h"
-
-//QT_BEGIN_NAMESPACE
-//namespace Ui { class Widget; }
-//QT_END_NAMESPACE
 
 class Widget : public QWidget
 {
@@ -22,17 +17,15 @@ public:
     ~Widget();
 
 private:
-    QVBoxLayout *m_pLayoutMain;
+    QGridLayout *m_pLayoutMain;
     QLabel* m_lableHash1;
     QLineEditCustom* m_LineEditCustomFilePath1;
     QLabel* m_lableHash2;
     QLineEditCustom* m_LineEditCustomFilePath2;
+    QLabel* m_lableResult;
     QPushButton* m_pBtnVerify;
 
-    QHBoxLayout *pHLayoutBtn;
-
 public slots:
-    void onLineEditChanged();
     void onBtnVerify();
 
 };
